dc7faf0b699c724ac0a43dbd4e41e452012f6e43
mumitrols <mumitrols@bk.ru>
2022-11-06 15:25:24 +0300

    В код добавлена библиотека WebDriverManager

--- src/test/java/ru/netology/web/DebitCardAppTest.java
+++ src/test/java/ru/netology/web/DebitCardAppTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.util.List;
 
@@ -20,18 +20,19 @@ class DebitCardAppTest {
 
     @BeforeAll
     public static void setUpAll(){
-        System.setProperty("webdriver.chrome.driver","driver/win/chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver","driver/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     void setUp (){
-       ChromeOptions options = new ChromeOptions();
-       options.addArguments("--disable-dev-shm-usage");
-       options.addArguments("--no-sandbox");
-       options.addArguments("--headless");
-       driver = new ChromeDriver(options);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
 
-       //driver =  new ChromeDriver();
+        //driver =  new ChromeDriver();
     }
 
     @Test
